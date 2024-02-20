bc0d951427d100f781609c2d1145baba5cae2947
PlumeSC <supawit.chukiat@gmail.com>
2024-02-20 09:42:25 +0700

    remove_core_api_utils_directories

--- modules/core/core_api/req_api.go
+++
@@ -1,9 +0,0 @@
-package coreapi
-
-type ApiRequest interface {
-	GetTeam(uint, uint, uint) (*Team, error)
-	GetLeague(league uint, season uint) (*League, error)
-	GetStandings(League uint, season uint) (*Standings, error)
-	GetPlayer(League uint, season uint, page int) (*Players, error)
-	GetFixture(League uint, season uint, round int) (*Match, error)
-}
